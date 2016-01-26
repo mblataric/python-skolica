aa2eb12cae0097f2d03de1cbe947405761e87c8a
Mario Blatarić <mario.blataric@gmail.com>
2016-01-26 08:51:44 +0100

    Dodani zadaci

--- 01-osnove.py
+++ 01-osnove.py
@@ -21,5 +21,15 @@ print ("A 3 plus 2 = ", 3 + 2)
 # U ovom podatku, unos spremimo u varijablu a koju poslije toga ispišemo
 
 a = input("Upiši svoje ime: ")
+
 print ("Tvoje ime je: ", a)
 
+
+# ZADATAK 1
+# Složi program koji će pitati koliko imaš godina. Nakon upisa, ispiši "Ti imaš <upisane godine> godina".
+# NAPOMENA: <upisane godine> zamijeni s podatkom koji je upisan
+
+# ZADATAK 2
+# Složi program koji će pitati za ime tate i mame
+# Nakon unosa ispiši: Tvoji roditelji se zovu <ime tate> i <ime mame>
+# NAPOMENA: <ime tate> i <ime mame> zamijeni s upisanim podacima
