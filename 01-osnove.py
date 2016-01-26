7c5e20d0dc3b2128c9f895b997071c3be924f6c8
Mario Blatarić <mario.blataric@gmail.com>
2016-01-26 08:34:32 +0100

    Upisane prve osnove

---
+++ 01-osnove.py
@@ -0,0 +1,25 @@
+# Ispis podatka na ekran
+
+# Ako želimo ispisati tekst, stavimo navodnike
+print ("Ovo je ispis teksta.")
+
+# Ako želimo ispisati broj, stavimo bez navodnika
+print (3)
+
+# Razlika između navodnika i bez:
+print ("3 + 2")
+print (3 + 2)
+
+# Možemo i kombinirati tekst i brojeve
+print ("Tekst '3 + 2': ", "3 + 2")
+print ("A 3 plus 2 = ", 3 + 2)
+
+
+
+# UNOS PODATAKA
+# Podatke unosimo s komandom input
+# U ovom podatku, unos spremimo u varijablu a koju poslije toga ispišemo
+
+a = input("Upiši svoje ime: ")
+print ("Tvoje ime je: ", a)
+
